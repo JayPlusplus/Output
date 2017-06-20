a38c8fbc49a0607d987b8e628ae466c22f8860e2
Jay Wu <wu274319426@gmail.com>
2017-06-20 10:26:42 -0400

    outputAssignment

---
+++ src/outputAssignment/Output.java
@@ -0,0 +1,34 @@
+package outputAssignment;
+	import java.util.Scanner;
+public class Output {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+		 Scanner sc = new Scanner(System.in);
+
+		    String firstName;
+		    int age;
+		    String height;
+		    double gpa;
+
+		    
+		    System.out.print( "What is your first name? " );
+		    firstName = sc.next();
+
+		    System.out.print( "How old are you? " );
+		    age = sc.nextInt();
+
+		    System.out.print( "How tall are you? " );
+		    height =  sc.next();
+
+		    System.out.print( "What is your GPA? " );
+		    gpa = sc.nextDouble();
+
+		    }
+		
+
+	}
+
+
